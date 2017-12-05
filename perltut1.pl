91a9328a47a488503181ebec446a1439699f8073
Bryan <bryanbuitrago86@gmail.com>
2017-12-05 20:38:41 +0000

    Perl Loops

--- perltut1.pl
+++ perltut1.pl
@@ -7,6 +7,7 @@ use diagnostics;
 use feature 'say';
 use feature "switch";
 
+
 # # CODE
 # print "Hello World\n";
 #
@@ -110,43 +111,91 @@ use feature "switch";
 # Boolean Operators: !, &&, ||
 
 # If, else if, else statements
-$age = 80;
-my $is_not_intoxicated = 1;
-my $age_last_exam = 16;
+# my $age = 80;
+# my $is_not_intoxicated = 1;
+# my $age_last_exam = 16;
+#
+# # Simple if example
+# if($age < 16){
+#   say "You can't drive";
+# } elsif(!$is_not_intoxicated) {
+#   say "You can't drive";
+# } else {
+#   say "You can drive";
+# }
+#
+# # Complex review of everything
+# if(($age >= 1) && ($age < 16)){
+#   say "You can't Drive";
+# } elsif(!$is_not_intoxicated){
+#   say "You can't drive";
+# } elsif(($age >= 80) && (($age > 100) || (($age - $age_last_exam) > 5))){
+#   say "You can't drive";
+# } else {
+#   say "You can drive";
+# }
+#
+# # Comparison operators for strings
+# # eq, ne, lt, le, gt, ge
+# if('a' eq 'b'){
+#   say "a equals b";
+# } else {
+#   say "a doesn't equal b";
+# }
+#
+# # unless is the opposite of if
+# unless(!$is_not_intoxicated){
+#   say "Get Sober";
+# }
+#
+# # Ternary operator returns different values depending
+# # on a condition
+# say (($age > 18) ? "Can Vote" : "Can't Vote");
 
-# Simple if example
-if($age < 16){
-  say "You can't drive";
-} elsif(!$is_not_intoxicated) {
-  say "You can't drive";
-} else {
-  say "You can drive";
-}
+# ---------- LOOPING ---------
 
-# Complex review of everything
-if(($age >= 1) && ($age < 16)){
-  say "You can't Drive";
-} elsif(!$is_not_intoxicated){
-  say "You can't drive";
-} elsif(($age >= 80) && (($age > 100) || (($age - $age_last_exam) > 5))){
-  say "You can't drive";
-} else {
-  say "You can drive";
-}
 
-# Comparison operators for strings
-# eq, ne, lt, le, gt, ge
-if('a' eq 'b'){
-  say "a equals b";
-} else {
-  say "a doesn't equal b";
-}
+# # for Loop
+# for(my $i = 0; $i < 10; $i++) {
+#   say $i;
+# }
+#
+# my $i = 1;
+#
+# # while Loop
+# while ($i < 10) {
+#   if($i % 2 == 0) {
+#     $i++;
+#
+#     next;
+#   }
+#
+#   if($i == 7){last;}
+#   say $i;
+#   $i++;
+# }
 
-# unless is the opposite of if
-unless(!$is_not_intoxicated){
-  say "Get Sober";
-}
+# # do while Loop
+# my $lucky_num = 7;
+# my $guess;
+# do {
+#   say "Guess a Number Between 1 and 10";
+#
+#   # This is how you get user input
+#   $guess = <STDIN>;
+# } while ($guess != $lucky_num);
+#
+# say "You guessed 7";
 
-# Ternary operator returns different values depending
-# on a condition
-say (($age > 18) ? "Can Vote" : "Can't Vote");
+# Given When Perl Switch statement (Javascript "switch" statement)
+# my $age_old = 18;
+# given ($age_old) {
+#   when($_ > 16) {
+#     say "Drive";
+#
+#     # Will continue with the next cases
+#     continue;
+#   }
+#   when($_ > 17) {say "Go Vote";}
+#   default {say "Nothing Special";}
+# }
