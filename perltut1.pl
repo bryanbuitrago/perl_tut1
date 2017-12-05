b1d309b001082cb03f4e8d987875aae4ca37d63f
Bryan <bryanbuitrago86@gmail.com>
2017-12-05 19:21:18 +0000

    Perl Math

--- perltut1.pl
+++ perltut1.pl
@@ -59,3 +59,46 @@ use feature "switch";
 # ($first, $second) = ($second, $first);
 #
 # say "$first $second";
+
+
+# ---------- MATH ----------
+
+say "5 + 4 = ", 5 + 4;
+say "5 - 4 = ", 5 - 4;
+say "5 * 4 = ", 5 * 4;
+say "5 / 4 = ", 5 / 4;
+say "5 % 4 = ", 5 % 4;
+say "5 ** 4 = ", 5 ** 4;
+
+say("=================");
+
+# Built in functions
+# Includes Trig functions plus
+say "EXP 1 = ", exp 1; # e to the power of
+say "HEX 10 = ", hex 10; # Convert from hexidecimal
+say "OCT 10 = ", oct 10; # Convert from Octal
+say "INT 6.45 = ", int(6.45); # Truncate You can use parentheses
+say "LOG 2 = ", log 2; # Number to the power of e
+say "Random between 0 - 10 = ", int(rand 11);
+
+say("=================");
+
+# Assignment Operators
+# +=, -=, *=, /=
+my $rand_num = 5;
+$rand_num += 1;
+say "Number Incremented ", $rand_num;
+
+say("=================");
+
+# Shortcut Increment and Decrement
+say "6++ = ", $rand_num++;
+say "++6 = ", ++$rand_num;
+say "6-- = ", $rand_num--;
+say "--6 = ", --$rand_num;
+
+say("=================");
+
+# Order of operations
+say "3 + 2 * 5 = ", 3 + 2 * 5;
+say "(3 + 2) * 5 = ", (3 + 2) * 5;
